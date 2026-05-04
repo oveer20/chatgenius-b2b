24d617aeaad572c7b6ca9bbfc821adec2b49b47a
oveer20 <oveer20@gmail.com>
2026-05-04 16:23:58 -0500

    Feat: Add Google Analytics 4 script to head

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -94,6 +94,16 @@ export default function RootLayout({
     <html lang="es" className={`${outfit.variable} ${inter.variable}`}>
       <head>
         <link rel="manifest" href="/manifest.json" />
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-C3EKFHJ4FE"></script>
+        <script dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-C3EKFHJ4FE');
+          `,
+        }} />
         {/* Google Tag Manager - for GTM container */}
         <script dangerouslySetInnerHTML={{
           __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-52MHDWQR');`,
