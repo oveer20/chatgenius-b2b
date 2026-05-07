7a31eb2ce3679ac52c987b32d0092914b6ecd0bd
oveer20 <oveer20@gmail.com>
2026-05-07 02:27:16 -0500

    fix: update Google Tag Manager ID to GTM-T8VQKWXX

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -107,7 +107,7 @@ export default function RootLayout({
         }} />
         {/* Google Tag Manager - for GTM container */}
         <script dangerouslySetInnerHTML={{
-          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-52MHDWQR');`,
+          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-T8VQKWXX');`,
         }} />
         
         {/* Schema.org - SoftwareApplication */}
@@ -224,7 +224,7 @@ export default function RootLayout({
           <ThemeEffect />
         {/* Google Tag Manager noscript */}
         <noscript dangerouslySetInnerHTML={{
-          __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-52MHDWQR" height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+          __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-T8VQKWXX" height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
         }} />
         {children}
         <FloatingWhatsApp />
